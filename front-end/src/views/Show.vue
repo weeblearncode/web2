cfd8600fff398f633df29d10d2bc8743b44c847f
Aaron <phucluongthevinh@gmail.com>
2025-08-03 15:34:47 +0700

    Initial commit of combined front-end and server

---
+++ front-end/src/views/Show.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Show Word</h1>
+    <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="germany flag"></i> German
+      </div>
+      <input type="text" readonly v-model="word.german" />
+    </div>
+
+    <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="united kingdom flag"></i> English
+      </div>
+      <input type="text" readonly v-model="word.english" />
+    </div>
+
+    <router-link :to="{ name: 'edit', params: { id: word._id }}" class="ui button">
+      Edit word
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { api } from '../helpers/helpers';
+
+export default {
+  name: 'show',
+  data: function() {
+    return {
+      word: {}
+    };
+  },
+  async mounted() {
+    this.word = await api.getWord(this.$route.params.id);
+  }
+};
+</script>
---
+++ front-end/src/views/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Vocabulary Test</h1>
+    <div class="ui segment">
+      <p>This is where you would test your vocabulary knowledge.</p>
+      <p>Component functionality to be implemented.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data() {
+    return {
+      // You can add test-related data here later
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add any component-specific styles here */
+.ui.segment {
+  margin-top: 20px;
+  padding: 20px;
+}
+</style>
